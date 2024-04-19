f31916dce7f895adcda3ec3918606e61804cc387
Michael Ekeghe <mekeghe@wisesystems.com>
2024-04-19 11:03:34 +0100

    third commit

--- moxieapi/urls.py
+++ moxieapi/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/v1/', include('medspa.urls')),
-    path('api/v1/', include('services.urls')),
+    path('api/v1/', include('service.urls')),
     path('api/v1/', include('appointment.urls')),
 ]
 
