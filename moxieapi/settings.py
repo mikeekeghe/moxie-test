2a43e0a046fa8ebfcc6dbddbd7e4c7570c7fcbf0
Michael Ekeghe <mekeghe@wisesystems.com>
2024-04-20 08:12:57 +0100

    after adding adocker file

--- moxieapi/settings.py
+++ moxieapi/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = "moxieapi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'my-postgres',  
+        'PORT': '5432',
     }
 }
+
 
 
 
