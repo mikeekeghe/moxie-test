5450bf035bdc922910b29300e171505578df95fa
Michael Ekeghe <mekeghe@wisesystems.com>
2024-04-21 09:31:28 +0100

    after adding adocker compose file

--- service/models.py
+++ service/models.py
@@ -6,7 +6,7 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration = models.DurationField()
+    duration = models.IntegerField() 
     medspa = models.ForeignKey(MedSpa, on_delete=models.CASCADE, related_name='services')
     appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, related_name='service_instances', null=True)
 
